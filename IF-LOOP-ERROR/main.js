0033be93d824dc8eb98fca9b5d95da54c02bb016
taku0622 <hayatsutkm@gmail.com>
2023-10-21 22:10:29 +0900

    done

--- IF-LOOP-ERROR/main.js
+++ IF-LOOP-ERROR/main.js
@@ -1,9 +1,12 @@
 let ok = false;
 let maybeOk = true;
-if (ok) {
-  console.log("OK!");
-} else if (maybeOk) {
-  console.log("maybe OK...");
-} else {
-  console.log("NO!");
-}
+// if (ok) {
+//   console.log("OK!");
+// } else if (maybeOk) {
+//   console.log("maybe OK...");
+// } else {
+//   console.log("NO!");
+// }
+
+ok = 1 === 1;
+console.log(ok);
