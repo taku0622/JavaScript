44fbb83c033850fa05ce9a271ad7e83395bc5bf1
taku0622 <hayatsutkm@gmail.com>
2023-10-21 21:15:43 +0900

    done

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 let count = 0;
-count = 3;
-count = 30;
+console.log(count);
 
+count = 3;
+console.log(count);
+
+count = 30;
+console.log(count);
+
+console.log("hello");
 const daysInWeek = 7;
 
 let number = 1;
