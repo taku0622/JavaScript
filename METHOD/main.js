30022b47c9e15d4f728cb72498b9e97c777418e0
taku0622 <hayatsutkm@gmail.com>
2023-11-18 21:03:30 +0900

    done

--- METHOD/main.js
+++ METHOD/main.js
@@ -1,17 +1,18 @@
-// function add(a, b) {
-//   return a + b;
-// }
+console.log(add(8, 9));
+function add(a, b) {
+  return a + b;
+}
 // console.log(add); // オブジェクトが返ってくる
 // console.log(add.name); // add
 // console.log(add.length); // 2 ※引数の数
 // const newAdd = add; // オブジェクトの代入
 // console.log(newAdd(3, 4)); // 7
 
+console.log(sayHi());     // hi
 const sayHi = function hi() { // 名前付き関数
   return "hi"
 }
-const sayHello = function () {  // 無名関数 
-  return "hello"
-}
-console.log(sayHi());     // hi
-console.log(sayHello());  // hello
+// const sayHello = function () {  // 無名関数
+//   return "hello"
+// }
+// console.log(sayHello());  // hello
