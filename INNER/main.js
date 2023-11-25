01f9ddc668b61beed23fe248afa9f3c3df9aba19
taku0622 <hayatsutkm@gmail.com>
2023-11-25 21:04:51 +0900

    done

--- INNER/main.js
+++ INNER/main.js
@@ -1,3 +1,10 @@
-'use strict'
-grape = "grape";
-console.log(grape); // Uncaught ReferenceError: grape is not defined
+// 'use strict'
+// grape = "grape";
+// console.log(grape); // Uncaught ReferenceError: grape is not defined
+
+const coffee = {
+  name: 'Caffe Latte'
+};
+const coffee2 = coffee;     // アドレス値の代入
+coffee2.name = 'Espresso';
+console.log(coffee.name);   // Espresso
