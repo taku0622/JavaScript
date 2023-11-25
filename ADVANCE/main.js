54f3a3fc597f05e095953c405622b760d7e10fc1
taku0622 <hayatsutkm@gmail.com>
2023-11-25 22:54:30 +0900

    done

--- ADVANCE/main.js
+++ ADVANCE/main.js
@@ -1,10 +1,34 @@
-let createCounter = () => {
-  let count = 0;
-  return () => {
-    count++;
-    console.log(count);
+// let createCounter = () => {
+//   let count = 0;
+//   return () => {
+//     count++;
+//     console.log(count);
+//   };
+// };
+// let counter = createCounter();
+// counter(); // 1
+// counter(); // 2
+
+let genetatePerson = (name) => {
+  let age = 0;
+  return {
+    getName: () => name,
+    getAge: () => age,
+    incrementAge: () => {
+      age++;
+    },
   };
 };
-let counter = createCounter();
-counter(); // 1
-counter(); // 2
+
+const jiro = genetatePerson('Jiro');
+console.log(jiro.getAge()); // 0
+jiro.incrementAge();
+jiro.incrementAge();
+console.log(jiro.getAge()); // 2
+console.log(jiro.getName());// Jiro
+
+const tom = genetatePerson('Tom');
+tom.incrementAge();
+tom.incrementAge();
+tom.incrementAge();
+console.log(tom.getAge());  // 3
