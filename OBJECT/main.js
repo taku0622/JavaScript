c6c600f8e00314919091e2d02b8117af83419eea
taku0622 <hayatsutkm@gmail.com>
2023-11-29 03:23:43 +0900

    done

--- OBJECT/main.js
+++ OBJECT/main.js
@@ -1,18 +1,26 @@
-const interests = 'interests';
-const person = {
-  name: 'John',
-  age: 30,
-  greeting: () => hello,
-  const: 'const',                     // 予約語
-  'current city': 'Tokyo',            // 文字列
-  3: 3,                               // 数値
-  3.1: 3.1,                           // 小数
-  '-4': -4,                           // 負は文字列のみ
-  [interests]: ['music', 'travel'],   // 変数
+// const interests = 'interests';
+// const person = {
+//   name: 'John',
+//   age: 30,
+//   greeting: () => hello,
+//   const: 'const',                     // 予約語
+//   'current city': 'Tokyo',            // 文字列
+//   3: 3,                               // 数値
+//   3.1: 3.1,                           // 小数
+//   '-4': -4,                           // 負は文字列のみ
+//   [interests]: ['music', 'travel'],   // 変数
+// };
+// person.name = 'Taku'; // 値変更
+// person.gender = 'man' // 値追加
+// delete person.age;    //  property(age)削除
+
+const name = 'Espresso';
+const size = 350;
+const coffee = {
+  name,
+  size,
 };
-person.name = 'Taku'; // 値変更
-person.gender = 'man' // 値追加
-delete person.age;    //  property(age)削除
+console.log(coffee);
 
 // console.log(person.name);       // John
 // console.log(person['name']);    // John
