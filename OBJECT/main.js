008114b0f8256ff2aaa3b5470ec343b86163da6c
taku0622 <hayatsutkm@gmail.com>
2023-11-29 02:49:46 +0900

    done

--- OBJECT/main.js
+++ OBJECT/main.js
@@ -10,16 +10,24 @@ const person = {
   '-4': -4,                           // 負は文字列のみ
   [interests]: ['music', 'travel'],   // 変数
 };
-console.log(person.name);       // John
-console.log(person['name']);    // John
-console.log(person.greeting);   // hello
-console.log(person['greeting']);// hello
-console.log(person.const);      // const
-console.log(person['const']);   // const
-console.log(person['current city']); // Tokyo
-console.log(person['3']);       // 3
-console.log(person[3]);         // 3
-console.log(person[3.1]);       // 3.1
-console.log(person['3.1']);     // 3.1
-console.log(person[interests]); // ['music', 'travel']
+// key一覧：['3', 'name', 'age', 'greeting', 'const', 'current city', '3.1', '-4', 'interests']
+console.log(Object.keys(person));
+// value一覧：[3, 'John', 30, ƒ, 'const', 'Tokyo', 3.1, -4, Array(2)]
+console.log(Object.values(person));
+// key&valueが配列で一覧：[[key, value],[key, value],[key, value]...]
+console.log(Object.entries(person));
+
+
+// console.log(person.name);       // John
+// console.log(person['name']);    // John
+// console.log(person.greeting);   // hello
+// console.log(person['greeting']);// hello
+// console.log(person.const);      // const
+// console.log(person['const']);   // const
+// console.log(person['current city']); // Tokyo
+// console.log(person['3']);       // 3
+// console.log(person[3]);         // 3
+// console.log(person[3.1]);       // 3.1
+// console.log(person['3.1']);     // 3.1
+// console.log(person[interests]); // ['music', 'travel']
 
