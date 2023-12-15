45560d577616ea02f39498d273c69ed26dafcff3
taku0622 <hayatsutkm@gmail.com>
2023-12-15 13:41:27 +0900

    done

--- OBJECT/main.js
+++ OBJECT/main.js
@@ -207,13 +207,17 @@
 // console.log(Object.prototype.hasOwnProperty.call(o, 'hello')) // 上の書き換え
 // console.log('hello' in o);              // true：prototypeまで確認する
 
-// class User {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-//   static greeting() {
-//     console.log('Hello');
-//   } // クラスメソッド
-// };
-// console.log(User.greeting()); // Hello
+class User {
+  id = 1993321;   // // フィールド
+  birthday = '1990/01//01'; // フィールド
+  static classId = 0;       // staticもいける
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greeting() { }
+};
+const user1 = new User('Taku', 25)
+console.log(user1.id);       // 1993321
+console.log(user1.birthday); // 1990/01//01
+console.log(User.classId);   // 0
