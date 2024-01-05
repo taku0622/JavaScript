556eec071947f6fba8cdd81fd2584eb69010f7c6
taku0622 <hayatsutkm@gmail.com>
2024-01-05 13:52:48 +0900

    done

--- BUILTIN/main.js
+++ BUILTIN/main.js
@@ -5,3 +5,11 @@ console.log(result); // 1.23
 let hello = 'hello';
 result = hello.toUpperCase(); // 大文字に変換
 console.log(result); // HELLO
+
+
+
+result = (10).toString(2); // 文字列に変換(2進数)
+console.log(result); // 1010
+
+result = Number.parseInt('1010', 2); // 数値変換(2進数→10進数)
+console.log(result); // 10
