6e1812f5aab95f38608cc2935f0c36b6669d06f7
taku0622 <hayatsutkm@gmail.com>
2024-02-21 10:52:37 +0900

    done

--- BROWSER/main.js
+++ BROWSER/main.js
@@ -1,3 +1,14 @@
 // console.log('hello');
 
-// alert('hello');
+// okボタンのダイアログ
+alert('hello');
+
+// ok/cancelボタンのダイアログ
+let result = confirm('Are you sure ?');
+// resultには、boolean値が返る
+console.log(result);
+
+// 入力欄、ok/cancelボタンのダイアログ
+result = prompt('name');
+// result
+console.log(result);
