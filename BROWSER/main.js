7630421968ef670ec6889a5bef031abf4af5fac0
taku0622 <hayatsutkm@gmail.com>
2024-02-25 06:00:20 +0900

    done

--- BROWSER/main.js
+++ BROWSER/main.js
@@ -28,3 +28,23 @@
 
 // // Screen {availWidth: 1920, availHeight: 1040, width: 1920, height: 1080, colorDepth: 24, …}
 // console.log(screen);
+
+// file:///C:/Users/★★★★★/Desktop/JavaScipt/BROWSER/index.html
+console.log(location.href);
+// file:
+console.log(location.protocol);
+// ドメイン、fileの場合はnull
+console.log(location.hostname);
+// https:443, http:80 左の場合、空文字
+console.log(location.port);
+// protocol://hostname:port
+console.log(location.origin);
+// hostname:port
+console.log(location.host);
+// hrelのhostname以降を返す。
+console.log(location.pathname);
+// urlに#が付いていた場合、それ以降を返す。
+console.log(location.hash);
+// クエリ(?)が付いていた場合、それ以降を返す。
+console.log(location.search);
+
