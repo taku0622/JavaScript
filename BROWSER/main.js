961c74e1fc288910e1256004343de90cc77b6ec9
taku0622 <hayatsutkm@gmail.com>
2024-02-25 05:37:52 +0900

    done

--- BROWSER/main.js
+++ BROWSER/main.js
@@ -1,14 +1,30 @@
 // console.log('hello');
 
 // okボタンのダイアログ
-alert('hello');
+// alert('hello');
 
 // ok/cancelボタンのダイアログ
-let result = confirm('Are you sure ?');
+// let result = confirm('Are you sure ?');
 // resultには、boolean値が返る
-console.log(result);
+// console.log(result);
 
 // 入力欄、ok/cancelボタンのダイアログ
-result = prompt('name');
+// result = prompt('name');
 // resultには入力欄の値が返る
-console.log(result);
+// console.log(result);
+
+// // Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+// console.log(navigator.userAgent);
+// // ja
+// console.log(navigator.language);
+// // Clipboard {}
+// console.log(navigator.clipboard);
+// // OSに接続されたカメラ、マイクの情報を取得
+// console.log(navigator.mediaDevices.getUserMedia);
+// // ブラウザに保存されたログイン情報を取得
+// console.log(navigator.credentials);
+// // 位置情報取得 GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1708806790143}
+// console.log(navigator.geolocation.getCurrentPosition(position => console.log(position)));
+
+// // Screen {availWidth: 1920, availHeight: 1040, width: 1920, height: 1080, colorDepth: 24, …}
+// console.log(screen);
