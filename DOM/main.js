2f3513233a42b17a0bed9e741941fac98eb1fb6a
taku0622 <hayatsutkm@gmail.com>
2024-07-27 17:34:49 +0900

    done

--- DOM/main.js
+++ DOM/main.js
@@ -5,4 +5,43 @@ result = document.head;
 // body要素
 result = document.body;
 
-console.dir(result);
+// 子ノードの取得
+result = document.childNodes;
+result = document.body.childNodes;
+
+// 子ノードを持っているか？
+result = document.body.childNodes[0].hasChildNodes();
+
+// 最初の子ノード
+result = document.body.firstChild;
+// 最後の子ノード
+result = document.body.lastChild;
+// 親ノード
+result = document.body.parentNode;
+
+// 隣のノード
+result = document.body.nextSibling;
+result = document.head.nextSibling.nextSibling;
+result = document.body.previousSibling.previousSibling;
+
+// 要素一覧取得
+result = document.body.children;
+// 始めの要素を取得
+result = document.body.firstElementChild;
+// 最後の要素を取得
+result = document.body.lastElementChild;
+// 親要素を取得
+result = document.body.parentElement;
+// 隣の要素を取得
+result = document.head.nextElementSibling;
+result = document.body.previousElementSibling;
+
+// 要素一覧取得 HTMLコレクションインタフェース
+colections = document.body.children;
+
+// ノード一覧取得 nodeリストインターフェース
+nodes = document.body.childNodes;
+
+// console.dir(result);
+console.dir(colections);
+console.dir(nodes);
